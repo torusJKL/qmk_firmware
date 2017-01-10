a075f4eb3c53b3d2f7d759c117ba4c0dc14d7777
Gal Buki <jkl@torus.ch>
2017-01-10 20:43:24 +0200

    there is no need for the left key in the digits layout

--- keyboards/planck/keymaps/torusjkl/keymap.c
+++ keyboards/planck/keymaps/torusjkl/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_DIGITS] = {
   {_______, _______, _______, _______, _______, _______, KC_NLCK,   KC_P7,   KC_P8,   KC_P9, KC_PAST, KC_PGUP},
-  {_______, _______, _______, _______, _______, _______, KC_LEFT,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PGDN},
+  {_______, _______, _______, _______, _______, _______, _______,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PGDN},
   {_______, _______, _______, _______, _______, _______, _______,   KC_P1,   KC_P2,   KC_P3, KC_PMNS, KC_PENT},
   {_______, _______, _______, _______, _______, _______, _______, _______,   KC_P0, KC_PDOT, KC_SLSH, KC_PENT}
 },
