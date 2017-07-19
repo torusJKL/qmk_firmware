07ab39e1d01b26e784e7536c16c2ebe3fb228cfe
Gal Buki (torusJKL) <jkl@torus.ch>
2017-07-19 18:57:17 +0300

    enhance fast typing with permissive hold

---
+++ keyboards/planck/keymaps/torusjkl/config.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+/*
+ * Tapping term & Permissive Hold 
+ */
+
+/* makes it easier for fast typists to use dual-function keys */
+#undef TAPPING_TERM
+#define TAPPING_TERM 200 //At 500 some bad logic takes hold
+#define PERMISSIVE_HOLD
+
+
+#endif
