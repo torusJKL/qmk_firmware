76b0b6b8b88b51b967dff7eaf5df34ed8457a476
Gal Buki (torusJKL) <jkl@torus.ch>
2017-08-12 23:35:10 +0300

    add Arrow layout (chord & pick activation)

--- keyboards/lets_split/keymaps/torusjkl/keymap.c
+++ keyboards/lets_split/keymaps/torusjkl/keymap.c
@@ -10,10 +10,12 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _DIGITS 4
+#define _ARROWS 5
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
-  DIGITS
+  DIGITS,
+  ARROWS,
 };
 
 // Fillers to make layering more clear
@@ -26,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |D/LT4 |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |A/LT5 |   S  |D/LT4 |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -35,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_ESC,  KC_A,    KC_S,LT(4,KC_D),    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_ESC,LT(5,KC_A),    KC_S,LT(4,KC_D),    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   _______,  KC_LCTL, KC_LALT, KC_LGUI, _______,   KC_SPC,  KC_SPC,  _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
@@ -56,6 +58,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______,    KC_4,    KC_5,    KC_6, KC_PLUS,  KC_ENT, \
   _______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3, KC_MINS,  KC_ENT, \
   _______, _______, _______, _______, _______, _______, _______, _______,    KC_0,  KC_DOT, KC_SLSH,  KC_EQL \
+),
+
+/* Arrows (chord and pick)
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      | HOME |  UP  |  END | PgUp |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | LEFT | DOWN |RIGHT | PgDn |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |SHOME |      | SEND |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ARROWS] = KEYMAP( \
+  _______, _______, _______, _______, _______, _______, _______, KC_HOME,   KC_UP,  KC_END, KC_PGUP, _______, \
+  _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, \
+  _______, _______, _______, _______, _______, _______, _______,S(KC_HOME),_______,S(KC_END),_______,_______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 )
 
 };
