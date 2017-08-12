bb8d7b4fbb27ba0320bc97e992db41d411f3277d
Gal Buki (torusJKL) <jkl@torus.ch>
2017-08-12 23:32:30 +0300

    adding the digits layer (chord & pick activation)

--- keyboards/lets_split/keymaps/torusjkl/keymap.c
+++ keyboards/lets_split/keymaps/torusjkl/keymap.c
@@ -9,9 +9,11 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
+#define _DIGITS 4
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE
+  QWERTY = SAFE_RANGE,
+  DIGITS
 };
 
 // Fillers to make layering more clear
@@ -24,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |   A  |   S  |D/LT4 |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -33,9 +35,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = KEYMAP( \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+  KC_ESC,  KC_A,    KC_S,LT(4,KC_D),    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
   _______,  KC_LCTL, KC_LALT, KC_LGUI, _______,   KC_SPC,  KC_SPC,  _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+),
+
+/* Digits (chord and pick)
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |   7  |   8  |   9  |   *  |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |   4  |   5  |   6  |   +  |Enter |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |   1  |   2  |   3  |   -  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |   0  |   .  |   /  |   =  |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_DIGITS] = KEYMAP( \
+  _______, _______, _______, _______, _______, _______, _______,    KC_7,    KC_8,    KC_9, KC_ASTR, _______, \
+  _______, _______, _______, _______, _______, _______, _______,    KC_4,    KC_5,    KC_6, KC_PLUS,  KC_ENT, \
+  _______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3, KC_MINS,  KC_ENT, \
+  _______, _______, _______, _______, _______, _______, _______, _______,    KC_0,  KC_DOT, KC_SLSH,  KC_EQL \
 )
 
 };
