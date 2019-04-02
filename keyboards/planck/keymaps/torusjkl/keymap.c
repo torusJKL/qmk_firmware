48b7201fe113b887009ee89b0704c0bf285cddce
Gal Buki <jkl@torus.ch>
2019-04-02 09:52:48 +0300

    make keymap compatible with upstream changes

--- keyboards/planck/keymaps/torusjkl/keymap.c
+++ keyboards/planck/keymaps/torusjkl/keymap.c
@@ -1,12 +1,8 @@
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,
 // this is the style you want to emulate.
 
-#include "planck.h"
-#include "action_layer.h"
-#ifdef AUDIO_ENABLE
-  #include "audio.h"
-#endif
-#include "eeconfig.h"
+#include QMK_KEYBOARD_H
+#include "muse.h"
 
 extern keymap_config_t keymap_config;
 
@@ -14,14 +10,16 @@ extern keymap_config_t keymap_config;
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-#define _QWERTY 0
-#define _NUMPAD 3
-#define _DIGITS 4
-#define _ARROWS 5
-#define _FUNCTIONS 6
-#define _SYMBOLS 7
-#define _SETTINGS 8
-#define _BRACKETS 9
+enum planck_layers {
+_QWERTY,
+_NUMPAD,
+_DIGITS,
+_ARROWS,
+_FUNCTIONS,
+_SYMBOLS,
+_SETTINGS,
+_BRACKETS
+};
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
@@ -36,7 +34,6 @@ enum planck_keycodes {
 };
 
 // Fillers to make layering more clear
-#define _______ KC_TRNS
 #define XXXXXXX KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -52,12 +49,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Ctrl | GUI  | Alt  | Hyper| Del  |    Space    | Bksp | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
-[_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    F(5),    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT},
-  {MT(MOD_LCTL, KC_ESC),  F(2),    F(4),    F(1),    F(3),    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MT(MOD_RCTL, KC_ENT)},
-  {KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC },
-  {KC_LCTL, KC_LGUI, KC_LALT, ALL_T(KC_NO), KC_DEL,   KC_SPC,  KC_SPC,  KC_BSPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
-},
+[_QWERTY] = LAYOUT_planck_grid(
+  KC_TAB,  KC_Q,    KC_W,    F(5),    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+  MT(MOD_LCTL, KC_ESC),  F(2),    F(4),    F(1),    F(3),    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MT(MOD_RCTL, KC_ENT),
+  KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+  KC_LCTL, KC_LGUI, KC_LALT, ALL_T(KC_NO), KC_DEL,   KC_SPC,  KC_SPC,  KC_BSPC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
 
 /* Numpad (chord and pick)
  * ,-----------------------------------------------------------------------------------.
@@ -70,12 +67,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |   0  |   .  |   /  |Enter |
  * `-----------------------------------------------------------------------------------'
  */
-[_NUMPAD] = {
-  {_______, _______, _______, _______, _______, _______, KC_NLCK,   KC_P7,   KC_P8,   KC_P9, KC_PAST, _______},
-  {_______, _______, _______, _______, _______, _______, _______,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PENT},
-  {_______, _______, _______, _______, _______, _______, _______,   KC_P1,   KC_P2,   KC_P3, KC_PMNS, KC_PENT},
-  {_______, _______, _______, _______, _______, _______, _______, _______,   KC_P0, KC_PDOT, KC_SLSH, KC_EQL}
-},
+[_NUMPAD] = LAYOUT_planck_grid(
+  _______, _______, _______, _______, _______, _______, KC_NLCK,   KC_P7,   KC_P8,   KC_P9, KC_PAST, _______,
+  _______, _______, _______, _______, _______, _______, _______,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PENT,
+  _______, _______, _______, _______, _______, _______, _______,   KC_P1,   KC_P2,   KC_P3, KC_PMNS, KC_PENT,
+  _______, _______, _______, _______, _______, _______, _______, _______,   KC_P0, KC_PDOT, KC_SLSH, KC_EQL
+),
 
 /* Digits (chord and pick)
  * ,-----------------------------------------------------------------------------------.
@@ -88,12 +85,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |   0  |   .  |   /  |   =  |
  * `-----------------------------------------------------------------------------------'
  */
-[_DIGITS] = {
-  {_______, _______, _______, _______, _______, _______, _______,    KC_7,    KC_8,    KC_9, KC_ASTR, _______},
-  {_______, _______, _______, _______, _______, _______, _______,    KC_4,    KC_5,    KC_6, KC_PLUS,  KC_ENT},
-  {_______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3, KC_MINS,  KC_ENT},
-  {_______, _______, _______, _______, _______, _______, _______, _______,    KC_0,  KC_DOT, KC_SLSH,  KC_EQL}
-},
+[_DIGITS] = LAYOUT_planck_grid(
+  _______, _______, _______, _______, _______, _______, _______,    KC_7,    KC_8,    KC_9, KC_ASTR, _______,
+  _______, _______, _______, _______, _______, _______, _______,    KC_4,    KC_5,    KC_6, KC_PLUS,  KC_ENT,
+  _______, _______, _______, _______, _______, _______, _______,    KC_1,    KC_2,    KC_3, KC_MINS,  KC_ENT,
+  _______, _______, _______, _______, _______, _______, _______, _______,    KC_0,  KC_DOT, KC_SLSH,  KC_EQL
+),
 
 /* Arrows (chord and pick)
  * ,-----------------------------------------------------------------------------------.
@@ -106,12 +103,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ARROWS] = {
-  {_______, _______, _______, _______, _______, _______, _______, KC_HOME,   KC_UP,  KC_END, KC_PGUP, _______},
-  {_______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______},
-  {_______, _______, _______, _______, _______, _______, _______,S(KC_HOME),_______,S(KC_END),_______,_______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-},
+[_ARROWS] = LAYOUT_planck_grid(
+  _______, _______, _______, _______, _______, _______, _______, KC_HOME,   KC_UP,  KC_END, KC_PGUP, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,
+  _______, _______, _______, _______, _______, _______, _______,S(KC_HOME),_______,S(KC_END),_______,_______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
 
 /* Functions (chord and pick)
  * ,-----------------------------------------------------------------------------------.
@@ -124,18 +121,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |Brite |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_FUNCTIONS] = {
-  {_______, _______, _______, _______, _______, _______, _______,   KC_F7,   KC_F8,   KC_F9,  KC_F12, _______},
-  {_______, _______, _______, _______, _______, _______, _______,   KC_F4,   KC_F5,   KC_F6,  KC_F11,KC_PSCREEN},
-  {_______, _______, _______, _______, _______, _______, _______,   KC_F1,   KC_F2,   KC_F3,  KC_F10, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, BACKLIT, _______, _______, _______}
-},
+[_FUNCTIONS] = LAYOUT_planck_grid(
+  _______, _______, _______, _______, _______, _______, _______,   KC_F7,   KC_F8,   KC_F9,  KC_F12, _______,
+  _______, _______, _______, _______, _______, _______, _______,   KC_F4,   KC_F5,   KC_F6,  KC_F11,KC_PSCREEN,
+  _______, _______, _______, _______, _______, _______, _______,   KC_F1,   KC_F2,   KC_F3,  KC_F10, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, BACKLIT, _______, _______, _______
+),
 
 /* Symbols (chord and pick)
-<<<<<<< HEAD
->>>>>>> Symbols layer based on pocketkk's Chord left Pick right design. No need for lower and raise layers anymore.
-=======
->>>>>>> b68204d83b67cee7e91a769ac93d294944da5c34
+Symbols layer based on pocketkk's Chord left Pick right design. No need for lower and raise layers anymore.
  * ,-----------------------------------------------------------------------------------.
  * |      |      |      |      |      |      |   \  |  &   |  *   |  (   |  )   |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -146,12 +140,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |  _   |  =   |  {   |  }   |  ~   |
  * `-----------------------------------------------------------------------------------'
  */
-[_SYMBOLS] = {
-  {_______, _______, _______, _______, _______, _______, KC_BSLS, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______},
-  {_______, _______, _______, _______, _______, _______, KC_MINS,  KC_DLR, KC_PERC, KC_CIRC, KC_PIPE, _______},
-  {_______, _______, _______, _______, _______, _______, KC_PLUS, KC_EXLM,   KC_AT, KC_HASH, KC_LBRC, KC_RBRC},
-  {_______, _______, _______, _______, _______, _______, _______, KC_UNDS,  KC_EQL, KC_LCBR, KC_RCBR, KC_TILD}
-},
+[_SYMBOLS] = LAYOUT_planck_grid(
+  _______, _______, _______, _______, _______, _______, KC_BSLS, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  _______, _______, _______, _______, _______, _______, KC_MINS,  KC_DLR, KC_PERC, KC_CIRC, KC_PIPE, _______,
+  _______, _______, _______, _______, _______, _______, KC_PLUS, KC_EXLM,   KC_AT, KC_HASH, KC_LBRC, KC_RBRC,
+  _______, _______, _______, _______, _______, _______, _______, KC_UNDS,  KC_EQL, KC_LCBR, KC_RCBR, KC_TILD
+),
 
 /* Settings
  * ,-----------------------------------------------------------------------------------.
@@ -164,12 +158,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_SETTINGS] = {
-  {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL},
-  {_______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______},
-  {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-},
+[_SETTINGS] = LAYOUT_planck_grid(
+  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______,
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
 
 /* Brackets (chord and pick)
  * ,-----------------------------------------------------------------------------------.
@@ -182,12 +176,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_BRACKETS] = {
-  {_______, _______, _______, _______, _______, _______, _______, KC_LABK, KC_LBRC, KC_RBRC, KC_RABK, _______},
-  {_______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, _______},
-  {_______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_BSLS, KC_SLSH, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
-},
+[_BRACKETS] = LAYOUT_planck_grid(
+  _______, _______, _______, _______, _______, _______, _______, KC_LABK, KC_LBRC, KC_RBRC, KC_RABK, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_PIPE, KC_BSLS, KC_SLSH, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
 
 };
 
@@ -220,7 +214,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case QWERTY:
       if (record->event.pressed) {
         #ifdef AUDIO_ENABLE
-          PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
+          stop_all_notes();
+          PLAY_SONG(tone_qwerty);
         #endif
         persistant_default_layer_set(1UL<<_QWERTY);
       }
@@ -251,14 +246,12 @@ void matrix_init_user(void) {
 
 void startup_user()
 {
-    _delay_ms(20); // gets rid of tick
-    PLAY_NOTE_ARRAY(tone_startup, false, 0);
+    PLAY_SONG(tone_startup);
 }
 
 void shutdown_user()
 {
-    PLAY_NOTE_ARRAY(tone_goodbye, false, 0);
-    _delay_ms(150);
+    PLAY_SONG(tone_goodbye);
     stop_all_notes();
 }
 
@@ -269,7 +262,7 @@ void music_on_user(void)
 
 void music_scale_user(void)
 {
-    PLAY_NOTE_ARRAY(music_scale, false, 0);
+    PLAY_SONG(music_scale);
 }
 
 #endif
